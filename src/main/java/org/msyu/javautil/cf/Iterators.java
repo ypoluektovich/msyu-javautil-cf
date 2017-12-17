6ab782287e1f4c9b8bdb68122c172470ff53bf08
Yanus Poluektovich <ypoluektovich@gmail.com>
2017-12-17 12:08:47 +0300

    add MapToCollection and a couple iterators

--- src/main/java/org/msyu/javautil/cf/Iterators.java
+++ src/main/java/org/msyu/javautil/cf/Iterators.java
@@ -3,6 +3,7 @@ package org.msyu.javautil.cf;
 import java.util.ArrayDeque;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
@@ -63,6 +64,125 @@ public class Iterators {
 	@SafeVarargs
 	public static <E> Iterator<E> concat(Iterator<E>... iterators) {
 		return concat(asList(iterators));
+	}
+
+	public static <E> Iterator<E> cutoff(Iterator<E> back, int count) {
+		return new CutoffIterator<>(back, count);
+	}
+
+	private static final class CutoffIterator<E> implements Iterator<E> {
+
+		private final Iterator<E> back;
+		private int remainingCount;
+
+		private E next;
+		private boolean hasNext;
+
+		CutoffIterator(Iterator<E> back, int count) {
+			this.back = back;
+			this.remainingCount = count;
+			updateNext();
+		}
+
+		private void updateNext() {
+			if (remainingCount == 0) {
+				hasNext = false;
+				next = null;
+				return;
+			}
+			if (back.hasNext()) {
+				hasNext = true;
+				next = back.next();
+				--remainingCount;
+			} else {
+				hasNext = false;
+				next = null;
+			}
+		}
+
+		@Override
+		public boolean hasNext() {
+			return hasNext;
+		}
+
+		@Override
+		public E next() {
+			if (!hasNext) {
+				throw new NoSuchElementException();
+			}
+			E elementToReturn = next;
+			updateNext();
+			return elementToReturn;
+		}
+
+	}
+
+	public static <E> ListIterator<E> reverseListIterator(List<E> list) {
+		return reverseListIterator(list.listIterator(list.size()), list.size());
+	}
+
+	public static <E> ListIterator<E> reverseListIterator(ListIterator<E> back, int size) {
+		return new ReverseListIterator<>(back, size);
+	}
+
+	private static final class ReverseListIterator<E> implements ListIterator<E> {
+
+		private final ListIterator<E> back;
+		private int size;
+
+		ReverseListIterator(ListIterator<E> back, int size) {
+			this.back = back;
+			this.size = size;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return back.hasPrevious();
+		}
+
+		@Override
+		public E next() {
+			return back.previous();
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return back.hasNext();
+		}
+
+		@Override
+		public E previous() {
+			return back.next();
+		}
+
+		@Override
+		public int nextIndex() {
+			return size - back.nextIndex();
+		}
+
+		@Override
+		public int previousIndex() {
+			return nextIndex() - 1;
+		}
+
+		@Override
+		public void remove() {
+			back.remove();
+			--size;
+		}
+
+		@Override
+		public void set(E e) {
+			back.set(e);
+		}
+
+		@Override
+		public void add(E e) {
+			back.add(e);
+			back.previous();
+			++size;
+		}
+
 	}
 
 }
